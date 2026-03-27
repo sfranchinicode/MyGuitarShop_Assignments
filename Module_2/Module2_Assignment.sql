62b4bc124cfacdd58731bfb441ae44f36ff50c44
sfranchinicode <sam.franchini92@gmail.com>
2026-03-26 18:01:58 -0700

    Add Module 2 queries: retrieve product details, full customer name, and filter by price and sorted by date

--- Module_2/Module2_Assignment.sql
+++ Module_2/Module2_Assignment.sql
@@ -1,2 +1,14 @@
 -- Module 2: My Guitar Shop Queries Continued
 
+-- 1. Run a SELECT statement to retrieve product details
+SELECT product_code, product_name, list_price, discount_percent
+FROM products;
+
+-- 2. Run a SELECT statment to retrieve full customer names
+SELECT CONCAT(last_name, ', ', first_name) AS full_name
+FROM customer
+WHERE last_name >= 'M'
+ORDER BY last_name ASC;
+
+-- 3. Run a SELECT statement to return products by price and sort by date
+SELECT
